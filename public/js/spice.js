3ecc66a9b7a215dab5128a972a67d9c27abac21b
JamesFW <james.wu@students.olin.edu>
2013-09-29 22:46:47 -0400

    site now works

--- public/js/spice.js
+++ public/js/spice.js
@@ -3,12 +3,16 @@ var Site = angular.module('Site', []);
 Site.config(function ($routeProvider) {
   $routeProvider
     .when('/home', {
-      templateUrl: 'home.html',
+      templateUrl: 'templates/home.html',
       controller: 'HomeController'
     })
     .when('/inspirations', {
-      templateUrl: 'inspirations.html',
+      templateUrl: 'templates/inspirations.html',
       controller: 'InspirationController'
+    })
+    .when('/brief', {
+      templateUrl: 'templates/brief.html',
+      controller: 'BriefController'
     })
     .otherwise({
       redirectTo: '/home'
@@ -19,11 +23,22 @@ function InspirationController ($scope, $http) {
   $http.get('public/data/inspirations.json').success(function (data) {
     $scope.inspirations = data;
   });
+  $scope.model = {
+    title: "Design Inspirations",
+    authors: "Kai Austin, Zachary Homans, James Wu"
+  }
 }
 
 function HomeController ($scope, $routeParams) {
   $scope.model = {
-    title: "Design Inspirations",
+    title: "Human Factors in Interface Design",
+    authors: "Kai Austin, Zachary Homans, James Wu"
+  }
+}
+
+function BriefController ($scope, $routeParams) {
+  $scope.model = {
+    title: "Project Brief",
     authors: "Kai Austin, Zachary Homans, James Wu"
   }
 }
